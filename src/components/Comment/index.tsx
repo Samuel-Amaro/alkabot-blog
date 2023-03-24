41acda6c1cafd6f3ebb2781a234ed153cd844ead
Samuel Amaro <samuelamaro96746313@gmail.com>
2023-03-24 14:17:46 -0300

    [style]: estilos mobile-first pagina users add

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -10,7 +10,7 @@ export default function Comment({comment} : PropsComment) {
     return (
       <>
         <div className="comment">
-          <Link to={"#"} target="_self" rel="next" className="comment__user">
+          <Link to={`#`} target="_self" rel="next" className="comment__user">
             {comment.email}
           </Link>
           <h3 className="comment__name">{comment.name}</h3>
